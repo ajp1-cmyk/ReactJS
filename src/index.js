7812129fce1e75e0df00673b70449e3c1c7ab922
ajp1-cmyk <74825515+ajp1-cmyk@users.noreply.github.com>
2022-08-22 18:58:06 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// var React = require("react");
+// var ReactDOM = require("react-dom");
+
+import React from "react";
+import ReactDOM from "react-dom";
+// ReactDOM.render(what to show , where to show)
+
+ReactDOM.render(<h1>Hello ✅ World!</h1>, document.getElementById("root"));
+// #vanilla js
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World 😈";
+
+// document.getElementById("root").appendChild(h1);
