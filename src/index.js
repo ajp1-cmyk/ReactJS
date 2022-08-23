269f7a80b0c85f7197e81f6942095f5aba926095
ajp1-cmyk <74825515+ajp1-cmyk@users.noreply.github.com>
2022-08-23 18:04:47 +0530

    modified elemts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// why react ??
+// to inject html files in our js code
+
 // var React = require("react");
 // var ReactDOM = require("react-dom");
 
@@ -5,9 +8,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 // ReactDOM.render(what to show , where to show)
 
-ReactDOM.render(<h1>Hello ✅ World!</h1>, document.getElementById("root"));
+//ReactDOM.render(<h1>Hello ✅ World!</h1>, document.getElementById("root"));
 // #vanilla js
 // let h1 = document.createElement("h1");
 // h1.innerHTML = "Hello World 😈";
 
 // document.getElementById("root").appendChild(h1);
+
+/* 
+what i want to create  multiple elements at same time?
+*/
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>this is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
